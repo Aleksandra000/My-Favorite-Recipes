59702932d185d4d873b8550612f6c36c2fcfe123
Aleksandra000 <ao.dobrowolska@gmail.com>
2020-04-19 12:43:53 +0200

    Updates

---
+++ precache-manifest.aaf9a0828029425bd73a9c2a9913055f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f64bd218bd110e6b9d032c56f2654a",
+    "url": "/My-Favorite-Recipes/index.html"
+  },
+  {
+    "revision": "6d347f07c4b3548641c8",
+    "url": "/My-Favorite-Recipes/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "993e92a2b713116c4505",
+    "url": "/My-Favorite-Recipes/static/css/main.f7ab3387.chunk.css"
+  },
+  {
+    "revision": "6d347f07c4b3548641c8",
+    "url": "/My-Favorite-Recipes/static/js/2.827bfc1e.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/My-Favorite-Recipes/static/js/2.827bfc1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993e92a2b713116c4505",
+    "url": "/My-Favorite-Recipes/static/js/main.a5034d60.chunk.js"
+  },
+  {
+    "revision": "e60a31defd8f73743633",
+    "url": "/My-Favorite-Recipes/static/js/runtime-main.76a4f395.js"
+  },
+  {
+    "revision": "d09280edd749f95a50f5f586137058cf",
+    "url": "/My-Favorite-Recipes/static/media/food.d09280ed.jpg"
+  }
+]);
